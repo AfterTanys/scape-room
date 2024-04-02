4e911412bcaf79e861081a0a721b38cb42763ae4
Daniel Rodríguez <dani_tanys@hotmail.com>
2024-04-02 19:44:58 +0200

    First Commit
    Primer Cambio

---
+++ js/pilgrimage.js
@@ -0,0 +1,11 @@
+function foyar(){
+    let pito = pene;
+    let tamaño = 3;
+    let sexo = "no, nadie me quiere";
+    let it = be;
+    let it = be;
+    let it = be;
+    let it = be;
+    Whisper words = "of wisdom";
+    let it = be;
+}
