8888095d31ca5360954d980f8606d21f475fdc72
Daniel Rodríguez <dani_tanys@hotmail.com>
2024-04-02 20:14:06 +0200

    Pongo Hola

--- js/pilgrimage.js
+++ js/pilgrimage.js
@@ -1,11 +1,3 @@
 function foyar(){
-    let pito = pene;
-    let tamaño = 3;
-    let sexo = "no, nadie me quiere";
-    let it = be;
-    let it = be;
-    let it = be;
-    let it = be;
-    Whisper words = "of wisdom";
-    let it = be;
+    let hola = "Hola";
 }
