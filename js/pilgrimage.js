2adc7a3a1843829a9c983f6defba2a63ebefe29a
Erizo14 <erizo14@hotmail.es>
2024-04-10 19:30:10 +0200

    Terminada la beta de la cinemática y del texto introductorio

--- js/pilgrimage.js
+++ js/pilgrimage.js
@@ -1,22 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
     const slides = [
-        { image: "../resources/img/intro1.webp", text: "loreipsumloreipsumloreipsumloreipsumloreipsumloreipsumloreipsumloreipsum" },
-        { image: "../resources/img/intro2.webp", text: "Texto de introducción 2." },
-        { image: "../resources/img/intro3.webp", text: "Texto de introducción 3." },
-        { image: "../resources/img/intro4.webp", text: "Texto de introducción 4." },
+        { 
+            image: "../resources/img/intro1.webp", 
+            text: "As the cataclysm claimed more lives, the need for salvation grew ever more desperate.\nWithout guidance from the savior of humanity, the Beacon Bearers found themselves compelled to seek answers on their own."
+        },
+        { 
+            image: "../resources/img/intro2.webp", 
+            text: "The last city of mankind decided it was time for an all-or-nothing gambit; they formed a group of their finest to gather fragments of the past that could save the future.\nTen were chosen to comprise the Final Squadron."
+        },
+        { 
+            image: "../resources/img/intro3.webp", 
+            text: "After years of relentless search, they pieced the puzzle together.\nAll clues led to one place: a sealed door in the depths of the ruins of a ship that once belonged to their God."
+        },
+        { 
+            image: "../resources/img/intro4.webp", 
+            text: "Their actions had not gone unnoticed.\nTime was of the essence, and the threat that nearly extinguished all life in the solar system, the Hostage, had pinpointed their location and deciphered their intentions.\nNow, the Final Squadron races against time to achieve their goal before the darkness looms over all once again."
+        }
     ];
 
     let currentSlide = 0;
     let textIndex = 0;
     let timeoutId = null;
 
-    function updateBackgroundImage() {
+    function updateBlurredBackground(imageSrc) {
+        const blurryBackground = document.getElementById('blurryBackground');
+        
+        blurryBackground.style.backgroundImage = `url(${imageSrc})`;
+        blurryBackground.style.backgroundSize = 'cover';
+        blurryBackground.style.backgroundPosition = 'center center';
+      }
+      
+     
+      
+      function updateBackgroundImage() {
         const backgroundImage = document.getElementById('backgroundImage');
         backgroundImage.src = slides[currentSlide].image;
+        updateBlurredBackground(slides[currentSlide].image); // Actualiza el fondo borroso
         document.getElementById('dialogBox').style.display = 'block';
         document.getElementById('dialogText').textContent = '';
         textIndex = 0;
-    }
+      }
 
     function typeWriter() {
         if (textIndex < slides[currentSlide].text.length) {
@@ -33,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
             timeoutId = null;
             textIndex = 0;
             currentSlide++;
-            if (currentSlide >= slides.length) {
+            if (currentSlide >= slides.length+1) {
                 window.location.href = "room1.html";
             }
             return;
