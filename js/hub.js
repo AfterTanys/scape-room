3d563c39a5037df9f5fde1707715f78f33230a63
Daniel Rodríguez <dani_tanys@hotmail.com>
2024-04-24 23:19:21 +0200

    Version 0.3: -Creado el Hub (Es el inxex.html que esta nada mas abres la carpeta) (Se ve bien a 1920x1080)(Falta por añadirle una animación a la ventana modal para forzar la pantalla completa) -Puesto puntero al puzzle de miguel -Borrado hubTest.html porque ya no sirve -Quitada la funcion de pantalla completa en la intro y movida al hub -Cambiado el js de intro.html para que salte a la cinematica del tiron -Añadidas 3 fotos de perfil

---
+++ js/hub.js
@@ -0,0 +1,33 @@
+const fotos = document.getElementsByClassName("hub-promotor-card-photo");
+fotos[0].style.backgroundImage="url(resources/img/miguel-photo.jpg)";
+fotos[1].style.backgroundImage="url(resources/img/eris-photo.png)";
+fotos[2].style.backgroundImage="url(resources/img/tanys-photo.png)";
+
+const overlay = document.getElementById("fullscreen-window-warning-overlay");
+
+document.getElementById('hub-start-button').addEventListener('click', function() {
+    openModal();
+    document.getElementById('hub-start-button').disabled=true;
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "F11") {
+    console.log("El usuario presionó F11 para pantalla completa.");
+    closeModal();
+    setTimeout(() => {
+        window.location.href = 'html/intro.html'; // Redirige a intro.html
+    }, 1000);
+  }
+});
+
+//Abre el modal
+function openModal() {
+  overlay.style.display="block";
+}
+
+//Cierra el modal
+function closeModal() {
+  overlay.style.display="none";
+}
+
+
--- js/intro.js
+++ js/intro.js
@@ -1,39 +1,4 @@
-/*
-Cambiar lo de abajo y mover todo el resto al HUB
 
 document.getElementById('startButton').addEventListener('click', function() {
-    //window.location.href = 'cinematic.html';
+    window.location.href = 'cinematic.html';
 });
-*/
-const overlay = document.getElementById("fullscreen-window-warning-overlay");
-const warning_window = document.getElementById("fullscreen-window-warning");
-
-document.getElementById('startButton').addEventListener('click', function() {
-    openModal(warning_window);
-    document.getElementById('startButton').disabled=true;
-    //window.location.href = 'cinematic.html';
-});
-
-document.addEventListener("keydown", function (event) {
-  if (event.key === "F11") {
-    console.log("El usuario presionó F11 para pantalla completa.");
-    closeModal(warning_window);
-    setTimeout(() => {
-        window.location.href = 'cinematic.html'; // Redirige a cinematic.html
-    }, 1000);
-  }
-});
-
-//Abre el modal
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-
-//Cierra el modal
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-}
