f3815b15506ff2755f8b322ac8b5027a24d65e35
Daniel Rodríguez <dani_tanys@hotmail.com>
2024-04-30 20:28:21 +0200

    Cambios: -Creado inventario (Aun no implementado solo aparece pero no es funcional y faltan las linternas) -Añadido inventario a Room1 y Room3 -Borrado codigo sobrante Room1 -Añadidos eventos en Room3 para cuando coges las bolas -Cambios en hud.js para implementar el boton de inventario (el boton de pista borra todo el inventario como testing hay que eliminar esa feature mas adelante) -cambiado nombre de las llaves del jeep a algo más comodo -Añadidas bolas y llaves del jeep en variantes oscuras

--- js/inventory.js
+++ js/inventory.js
@@ -1,7 +1,90 @@
-//INVENTARIO TESTING  NO METER 
-//Al principio del juego hay que reiniciar absolutamente todo el inventario y se ira ganando cosas
 
-//Borrado de bolas
-if(localStorage.getItem('bolas')){
-    localStorage.removeItem("bolas");
+let slideBallsItem;
+let crystalariumLanternItem;
+let jeepKeysItem;
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Comprobación de items
+  if (localStorage.getItem("slideBallsItem")) {
+    slideBallsItem = JSON.parse(localStorage.getItem("slideBallsItem"));
+  } else {
+    slideBallsItem = 0;
+    localStorage.setItem("slideBallsItem", JSON.stringify(slideBallsItem));
+  }
+
+  if (localStorage.getItem("jeepKeys")) {
+    jeepKeysItem = JSON.parse(localStorage.getItem("jeepKeys"));
+  } else {
+    jeepKeysItem = 0;
+    localStorage.setItem("jeepKeys", JSON.stringify(jeepKeysItem));
+  }
+
+  if (localStorage.getItem("crystalariumLantern")) {
+    crystalariumLanternItem = JSON.parse(
+      localStorage.getItem("crystalariumLantern")
+    );
+  } else {
+    crystalariumLanternItem = 0;
+    localStorage.setItem(
+      "crystalariumLantern",
+      JSON.stringify(crystalariumLanternItem)
+    );
+  }
+
+  //Creacion del modal
+  const inventoryCanvas = document.getElementsByTagName("body")[0];
+  let inventoryModal = document.createElement("div");
+  inventoryModal.classList.add("modal-puzzle");
+  inventoryModal.id = "inventory-modal";
+  inventoryCanvas.appendChild(inventoryModal);
+
+  //Creación de items individuales
+  let inventoryItem1 = document.createElement("div");
+  inventoryItem1.classList.add("inventory-item");
+  inventoryModal.appendChild(inventoryItem1);
+
+  if (slideBallsItem < 4) {
+    inventoryItem1.style.backgroundImage =
+      "url('../resources/img/inventory-balls-empty.png')";
+    inventoryItem1.innerHTML = slideBallsItem;
+  } else {
+    inventoryItem1.style.backgroundImage =
+      "url('../resources/img/inventory-balls.png')";
+  }
+
+  let inventoryItem2 = document.createElement("div");
+  inventoryItem2.classList.add("inventory-item");
+  inventoryModal.appendChild(inventoryItem2);
+
+  if (crystalariumLanternItem != 0) {
+    //Linterna encendida
+  } else {
+    //Linterna apagada
+  }
+
+  let inventoryItem3 = document.createElement("div");
+  inventoryItem3.classList.add("inventory-item");
+  inventoryModal.appendChild(inventoryItem3);
+
+  if (jeepKeysItem != 0) {
+    inventoryItem3.style.backgroundImage =
+      "url('../resources/img/jeep-keys.png')";
+  } else {
+    inventoryItem3.style.backgroundImage =
+      "url('../resources/img/jeep-keys-empty.png')";
+  }
+});
+
+function openInventory() {
+  openModal(document.getElementById("inventory-modal"));
+}
+
+function closeInventory() {
+  closeModal(document.getElementById("inventory-modal"));
+}
+//Con esta funcion borras todo lo que haya en el inventario
+function resetInventory(){
+    localStorage.removeItem("slideBallsItem");
+    localStorage.removeItem("jeepKeys");
+    localStorage.removeItem("crystalariumLantern");
 }
