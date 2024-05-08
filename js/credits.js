49e1db230e25ec4ca5c2f23f09ea4458e10dbce4
Erizo14 <erizo14@hotmail.es>
2024-05-08 20:19:42 +0200

    tamaño aumentado

--- js/credits.js
+++ js/credits.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // You might want to add some interactivity after the credits finish
     const credits = document.querySelector('.credits-container');
     credits.addEventListener('animationend', () => {
-        alert("HORACIO IS ALWAYS WATCHING...");
+        window.location.href = '../index.html';
     });
 });
